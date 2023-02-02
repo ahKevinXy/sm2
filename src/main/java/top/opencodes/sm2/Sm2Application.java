fe9c1c7821514597c07c21495fee08a5000030a3
ahKevinXy <2502329620@qq.com>
2023-02-02 18:17:11 +0800

    init

---
+++ src/main/java/top/opencodes/sm2/Sm2Application.java
@@ -0,0 +1,17 @@
+package top.opencodes.sm2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+* 应用启动入口
+*
+* */
+@SpringBootApplication
+public class Sm2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sm2Application.class, args);
+	}
+
+}
---
+++ src/test/java/top/opencodes/sm2/Sm2ApplicationTests.java
@@ -0,0 +1,13 @@
+package top.opencodes.sm2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sm2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
